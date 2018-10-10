608c6fa64704450540e74952cfa1dc7121d347cc
c.mcguire <cdmcguire000@gmail.com>
2018-10-10 15:44:15 -0700

    added body parsing and cors error handling

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const express = require('express');
 const app = express();
 // Logging
 const morgan = require('morgan');
+// Body Parser
+const bodyParser = require('body-parser');
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS') {
+        res.header('Acces-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+});
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+// Logs information about the browser request
 app.use(morgan('dev'));
+//
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
